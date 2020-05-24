b46a3f96a47bf9424971f56518a6fd14bdb83fd6
Roy Barak <barakroy@gmail.com>
2020-05-24 09:03:23 +0300

    Add files via upload

---
+++ app/app.js
@@ -0,0 +1,21 @@
+﻿
+var app = angular.module("fileManagerApp", [
+  'ngRoute',
+	'ngMaterial',
+]);
+
+app.config(function ($routeProvider ,$locationProvider) {
+ 
+
+  // Remove # from URL
+  $locationProvider.html5Mode(true);
+
+  
+  
+    $routeProvider
+        .when('/',
+        {
+            templateUrl: 'views/home.html'
+        })
+    .otherwise({ redirectTo: '/' });
+});
